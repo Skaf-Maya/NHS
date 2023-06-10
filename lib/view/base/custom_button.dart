ab4f499727d6e22308872364a85232750e7e71b3
@maya.skaf <maya.skaf@axtro-soft.com>
2023-06-11 02:51:51 +0300

    some changes

--- lib/view/base/custom_button.dart
+++ lib/view/base/custom_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:nhs/theme/light_theme.dart';
 
 class CustomButton extends StatelessWidget {
